fix rankings calculation;

src/models.py
@@ -17,9 +17,6 @@ class TTransE(nn.Module):
         nn.init.xavier_uniform_(self.e_embed.weight)
         nn.init.xavier_uniform_(self.r_embed.weight)
         nn.init.xavier_uniform_(self.t_embed.weight)
-        self.e_embed.weight.data = F.normalize(self.e_embed.weight, p=2, dim=1)
-        self.r_embed.weight.data = F.normalize(self.r_embed.weight, p=2, dim=1)
-        self.t_embed.weight.data = F.normalize(self.t_embed.weight, p=2, dim=1)
 
     def forward(self, pos_s, pos_r, pos_o, pos_t, neg_s, neg_r, neg_o, neg_t):
         pos_s_e = self.e_embed(pos_s)
@@ -49,11 +46,11 @@ class TAX(nn.Module):
         self.lstm = nn.LSTM(args.embedding_size, args.embedding_size, num_layers=1, batch_first=True)
         for name, param in self.lstm.named_parameters():
             if 'weight_ih' in name:
-                nn.init.xavier_uniform_(param.data)
+                nn.init.xavier_uniform_(param)
             elif 'weight_hh' in name:
-                nn.init.orthogonal_(param.data)
+                nn.init.orthogonal_(param)
             elif 'bias' in name:
-                nn.init.zeros_(param.data)
+                nn.init.zeros_(param)
 
         self.e_embed = nn.Embedding(e_cnt, args.embedding_size)
         self.r_embed = nn.Embedding(r_cnt, args.embedding_size)
@@ -61,9 +58,6 @@ class TAX(nn.Module):
         nn.init.xavier_uniform_(self.e_embed.weight)
         nn.init.xavier_uniform_(self.r_embed.weight)
         nn.init.xavier_uniform_(self.t_embed.weight)
-        self.e_embed.weight.data = F.normalize(self.e_embed.weight, p=2, dim=1)
-        self.r_embed.weight.data = F.normalize(self.r_embed.weight, p=2, dim=1)
-        self.t_embed.weight.data = F.normalize(self.t_embed.weight, p=2, dim=1)
 
     def rt_embed(self, r, t):
         r_e = self.r_embed(r)

src/utils.py
@@ -176,9 +176,9 @@ def evaluate(args, b, mdl, mtr, dvc):
             s_r = torch.matmul(ort, mdl.module.e_embed.weight.t()).argsort(dim=1, descending=True).numpy()
         else:
             ort = o_embed - rt_embed
-            s_r = torch.cdist(mdl.module.e_embed.weight, ort, p=_p(args)).argsort(dim=1, descending=True).numpy().T
+            s_r = torch.cdist(mdl.module.e_embed.weight, ort, p=_p(args)).t().argsort(dim=1, descending=True).numpy()
         for i, s in enumerate(b[:, 0]):
-            mtr.update(np.searchsorted(s_r[i], s) + 1)
+            mtr.update(np.argwhere(s_r[i] == s)[0, 0] + 1)
 
     if args.mode != 'head':
         ts_s = torch.LongTensor(b[:, 0]).to(dvc)
@@ -190,7 +190,7 @@ def evaluate(args, b, mdl, mtr, dvc):
             srt = s_embed + rt_embed
             o_r = torch.cdist(srt, mdl.module.e_embed.weight, p=_p(args)).argsort(dim=1, descending=True).numpy()
         for i, o in enumerate(b[:, 2]):
-            mtr.update(np.searchsorted(o_r[i], o) + 1)
+            mtr.update(np.argwhere(o_r[i] == o)[0, 0] + 1)
 
 
 def save(args, mdl):