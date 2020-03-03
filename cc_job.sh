9ddb8b6d941895879da7c23e07e7aef808abf2cf
Kian Ahrabian <kian.ahrabian@mail.mcgill.ca>
2020-03-02 23:57:10 -0500

    add timeline

--- cc_job.sh
+++ cc_job.sh
@@ -12,21 +12,21 @@
 
 module load python/3.7.4 gcc/7.3.0 cuda/10.0.130 cudnn/7.6 openmpi/3.1.2 nccl/2.3.5
 
-source $VENVDIR/gg/bin/activate
+source ${VENVDIR}/gg/bin/activate
 
-horovodrun -np 2 -H localhost:2020 main.py \
-       -ds GitGraph \
-       -m TADistMult \
-       -d 0.2 \
-       -l1 \
-       -es 256 \
-       -lr 0.001 \
-       -e 100 \
-       -bs 4096 \
-       -ns 64 \
-       -f \
-       -fp \
-       -as \
-       -md head \
-       -lf 10 \
-       -w 10
+horovodrun -np 2 -H localhost:2020 --timeline-filename ./logs/timeline-${SLURM_JOB_ID}.json --timeline-mark-cycles main.py \
+           -ds GitGraph \
+           -m TADistMult \
+           -d 0.2 \
+           -l1 \
+           -es 256 \
+           -lr 0.001 \
+           -e 100 \
+           -bs 4096 \
+           -ns 64 \
+           -f \
+           -fp \
+           -as \
+           -md head \
+           -lf 10 \
+           -w 10
